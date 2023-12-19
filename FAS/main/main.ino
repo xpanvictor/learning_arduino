cfa462c945444166947d47b51e28aa8e22ef5ded
xpanvictor <xpanvictor@gmail.com>
2023-12-19 23:16:42 +0100

    feat(init): initializes FAS

---
+++ FAS/main/main.ino
@@ -0,0 +1,19 @@
+"""
+A FAS built on a distributed system
+Design structure: 
+++++
+
+---
+Author: https://github.com/xpanvictor
+"""
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
